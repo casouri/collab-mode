c7834fe74e4376735bbfa0cd81abab704cf3f084
Yuan Fu <casouri@gmail.com>
2025-05-09 23:40:33 -0700

    Convert to workspace

--- collab_mode/src/file_server.rs
+++ collab_mode/src/file_server.rs
@@ -1,9 +1,13 @@
-use crate::{error::CollabResult, fatal, nfatal, types::JsonMap};
+use crate::types::JsonMap;
 use anyhow::{anyhow, Context};
 use rusqlite::Connection;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
+// *** Struct
+
+/// A shared directory, basically. Remote user can freely browse and
+/// open files under a shared project.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Project {
     /// Name of the project, default to the base name of `root`.
@@ -14,6 +18,7 @@ pub struct Project {
     meta: JsonMap,
 }
 
+/// File in a project.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProjectFile {
     /// Relative path of this file under its project.
@@ -25,6 +30,13 @@ pub struct ProjectFile {
     /// the UI, we still check the actual file ourselves.
     is_symlink: bool,
 }
+
+#[derive(Debug)]
+pub struct FileServer {
+    db: rusqlite::Connection,
+}
+
+// *** Impl
 
 impl ProjectFile {
     /// Get the basename of the file.
@@ -49,12 +61,19 @@ impl ProjectFile {
     }
 }
 
-const CREATE_PROJECTS_TABLE: (&'static str) = "CREATE TABLE IF NOT EXISTS projects (
+// *** Const
+
+const CREATE_PROJECTS_TABLE: &'static str = "CREATE TABLE IF NOT EXISTS projects (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 root TEXT NOT NULL UNIQUE,
 name TEXT NOT NULL,
 meta TEXT NOT NULL
 )";
+
+const ADD_PROJECT: &'static str = "INSERT INTO projects (root, name, meta)
+VALUES (?, ?, ?)";
+
+// *** Fn
 
 /// Initialize the DB `conn`. Create necessary tables.
 pub fn init_db(conn: Connection) -> anyhow::Result<()> {
@@ -91,9 +110,27 @@ pub fn listdir(project: Project, rel_path: &str) -> anyhow::Result<Vec<ProjectFi
     Ok(result)
 }
 
+/// Get file content of `file` under `project`.
 pub fn file_content(project: &Project, file: &ProjectFile) -> anyhow::Result<String> {
     let filename = file
         .abs_filename(project)
         .with_context(|| format!("Failed to access file {}", file.rel_path))?;
     std::fs::read_to_string(filename).with_context(|| format!("Can't read {}", file.rel_path))
+}
+
+/// Create a project at `path`.
+pub fn add_project(path: &str, conn: Connection) -> anyhow::Result<Project> {
+    let filename = PathBuf::from(path);
+    let basename = if let Some(name) = filename.file_name() {
+        name.to_string_lossy().to_string()
+    } else {
+        return Err(anyhow!("Can't resolve the filename of {}", path));
+    };
+    conn.execute(ADD_PROJECT, (&basename, path, "{}"))
+        .with_context(|| format!("Can't insert new project to DB"))?;
+    Ok(Project {
+        name: basename,
+        root: path.to_string(),
+        meta: serde_json::from_str("{}").unwrap(),
+    })
 }
