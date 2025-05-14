69c719d0eb0b5a602f02a1bb09c717091625c044
Yuan Fu <casouri@gmail.com>
2025-05-14 00:49:35 -0700

    Add list_files and test server

--- collab_mode/src/file_server.rs
+++ collab_mode/src/file_server.rs
@@ -1,4 +1,4 @@
-use crate::types::JsonMap;
+use crate::{collab_doc::Doc, types::JsonMap};
 use anyhow::{anyhow, Context};
 use rusqlite::Connection;
 use serde::{Deserialize, Serialize};
@@ -9,17 +9,21 @@ use std::path::PathBuf;
 /// A shared directory, basically. Remote user can freely browse and
 /// open files under a shared project.
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Project {
+    /// Id of the project.
+    id: u64,
     /// Name of the project, default to the base name of `root`.
     name: String,
     /// Absolute path of the root of the project.
     root: String,
-    /// Metadata for the project.
-    meta: JsonMap,
+    /// Metadata for the project. A JSON string.
+    meta: String,
 }
 
 /// File in a project.
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ProjectFile {
     /// Relative path of this file under its project.
     rel_path: String,
@@ -29,11 +33,6 @@ pub struct ProjectFile {
     /// Whether this file is a symlink. This is only for showing in
     /// the UI, we still check the actual file ourselves.
     is_symlink: bool,
-}
-
-#[derive(Debug)]
-pub struct FileServer {
-    db: rusqlite::Connection,
 }
 
 // *** Impl
@@ -63,27 +62,90 @@ impl ProjectFile {
 
 // *** Const
 
-const CREATE_PROJECTS_TABLE: &'static str = "CREATE TABLE IF NOT EXISTS projects (
+const CREATE_PROJECT_TABLE: &'static str = "CREATE TABLE IF NOT EXISTS project (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-root TEXT NOT NULL UNIQUE,
 name TEXT NOT NULL,
+root TEXT NOT NULL UNIQUE,
 meta TEXT NOT NULL
 )";
 
-const ADD_PROJECT: &'static str = "INSERT INTO projects (root, name, meta)
-VALUES (?, ?, ?)";
+const CREATE_DOC_TABLE: &'static str = "CREATE TABLE IF NOT EXISTS doc (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+abs_path TEXT NOT NULL,
+site_seq INTEGER NOT NULL,
+snapshot_internal_doc TEXT NOT NULL,
+snapshot_content TEXT NOT NULL,
+snapshot_seq INTEGER NOT NULL,
+meta TEXT NOT NULL
+)";
+
+const CREATE_OP_TABLE: &'static str = "CREATE TABLE IF NOT EXISTS op (
+doc_id INTEGER NOT NULL,
+seq INTEGER NOT NULL,
+site_seq INTEGER NOT NULL,
+group_seq INTEGER NOT NULL,
+kind INTEGER NOT NULL,
+op TEXT NOT NULL,
+remote INTEGER NOT NULL,
+FOREIGN KEY(doc_id) REFERENCES doc(id)
+)";
 
 // *** Fn
 
 /// Initialize the DB `conn`. Create necessary tables.
-pub fn init_db(conn: Connection) -> anyhow::Result<()> {
-    conn.execute(CREATE_PROJECTS_TABLE, ())
-        .with_context(|| format!("Failed to create table in DB"))?;
+pub fn init_db(conn: &Connection) -> anyhow::Result<()> {
+    conn.execute(CREATE_PROJECT_TABLE, ())
+        .with_context(|| format!("Failed to initialize DB"))?;
+    conn.execute(CREATE_DOC_TABLE, ())
+        .with_context(|| format!("Failed to initialize DB"))?;
     Ok(())
 }
 
+pub fn list_projects(conn: &Connection) -> anyhow::Result<Vec<Project>> {
+    let mut stmt = conn.prepare("SELECT id, name, root, meta FROM project")?;
+    let projects_iter = stmt
+        .query_map([], |row| {
+            Ok(Project {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                root: row.get(2)?,
+                meta: row.get(3)?,
+            })
+        })
+        .with_context(|| format!("Failed to query DB for projects"))?;
+
+    let mut projects: Vec<Project> = vec![];
+    for proj in projects_iter {
+        projects.push(proj.with_context(|| format!("Failed to query DB"))?);
+    }
+    Ok(projects)
+}
+
+pub fn list_files(
+    project_id: u64,
+    rel_path: &str,
+    conn: &Connection,
+) -> anyhow::Result<Vec<ProjectFile>> {
+    let project = conn
+        .query_row_and_then(
+            "SELECT id, name, root, meta FROM project WHERE id = ?",
+            [project_id],
+            |row| {
+                Ok(Project {
+                    id: row.get(0)?,
+                    name: row.get(1)?,
+                    root: row.get(2)?,
+                    meta: row.get(3)?,
+                }) as anyhow::Result<Project>
+            },
+        )
+        .with_context(|| format!("Failed to get the project (id = {}) from DB", project_id))?;
+    list_files_1(project, rel_path)
+}
+
 /// List the files under the directory at `rel_path` in `project`.
-pub fn listdir(project: Project, rel_path: &str) -> anyhow::Result<Vec<ProjectFile>> {
+pub fn list_files_1(project: Project, rel_path: &str) -> anyhow::Result<Vec<ProjectFile>> {
+    let rel_path = rel_path.trim_start_matches('/');
     let filename = std::path::Path::new(&project.root).join(rel_path);
     let filename_str = filename.to_string_lossy().to_string();
     let stat = std::fs::metadata(&filename)
@@ -119,18 +181,43 @@ pub fn file_content(project: &Project, file: &ProjectFile) -> anyhow::Result<Str
 }
 
 /// Create a project at `path`.
-pub fn add_project(path: &str, conn: Connection) -> anyhow::Result<Project> {
-    let filename = PathBuf::from(path);
-    let basename = if let Some(name) = filename.file_name() {
+pub fn add_project(path: &str, conn: &Connection) -> anyhow::Result<Project> {
+    let abs_path = expanduser::expanduser(path)
+        .with_context(|| format!("Can't expand filename to absolute path"))?;
+    let basename = if let Some(name) = abs_path.file_name() {
         name.to_string_lossy().to_string()
     } else {
         return Err(anyhow!("Can't resolve the filename of {}", path));
     };
-    conn.execute(ADD_PROJECT, (&basename, path, "{}"))
-        .with_context(|| format!("Can't insert new project to DB"))?;
+    conn.execute(
+        "INSERT INTO project (name, root, meta) VALUES (?, ?, ?)",
+        (&basename, abs_path.to_str(), "{}"),
+    )
+    .with_context(|| format!("Can't insert new project to DB"))?;
     Ok(Project {
+        id: conn.last_insert_rowid() as u64,
         name: basename,
-        root: path.to_string(),
-        meta: serde_json::from_str("{}").unwrap(),
+        root: abs_path.to_string_lossy().to_string(),
+        meta: "{}".to_string(),
     })
+}
+
+/// Starting tracking a file as a doc.
+pub fn add_doc(
+    file: &ProjectFile,
+    project: &Project,
+    conn: &mut Connection,
+) -> anyhow::Result<Doc> {
+    // let abs_path = file.abs_filename(project).with_context(|| {
+    //     format!(
+    //         "Failed to resolve absolute path of file {} in project {}",
+    //         file.rel_path, project.name
+    //     )
+    // })?;
+    // let stmt = conn.prepare(
+    //     "SELECT id, abs_path, site_seq, snapshot_interal_doc, snapshot_content, snapshot_seq, meta FROM doc WHERE abs_path = ?",
+    // );
+    // stmt.query_map(vec![abs_path], |row| {})
+    //     .with_context(|| format!("Failed to find doc in DB"));
+    todo!()
 }
---
+++ test_server/src/main.rs
@@ -0,0 +1,145 @@
+use std::collections::HashMap;
+
+use anyhow::Context;
+use collab_mode::{
+    config_man,
+    file_server::{self, Project},
+};
+use serde::Serialize;
+use tiny_http::{Method, Request, Response, Server, StatusCode};
+use url::{ParseError, Url};
+
+fn send_400_missing(param_name: &str, request: Request) -> anyhow::Result<()> {
+    request.respond(Response::new(
+        400.into(),
+        vec![],
+        format!("{{\"message\": \"Missing param `{}`\"}}", param_name).as_bytes(),
+        None,
+        None,
+    ))?;
+    Ok(())
+}
+
+fn send_400_error(message: &str, request: Request) -> anyhow::Result<()> {
+    request.respond(Response::new(
+        400.into(),
+        vec![],
+        format!("{{\"message\": \"{}\"}}", message).as_bytes(),
+        None,
+        None,
+    ))?;
+    Ok(())
+}
+
+/// Respond to `request` with 200 if `resp` is `Ok`; respond with 500
+/// otherwise.
+fn respond_200_or_500<E: ToString>(
+    resp: Result<String, E>,
+    request: Request,
+) -> anyhow::Result<()> {
+    match resp {
+        Ok(val) => {
+            request.respond(Response::new(
+                200.into(),
+                vec![],
+                val.as_bytes(),
+                None,
+                None,
+            ))?;
+        }
+        Err(err) => {
+            request.respond(Response::new(
+                500.into(),
+                vec![],
+                format!("{{\"error\": \"{}\"}}", err.to_string()).as_bytes(),
+                None,
+                None,
+            ))?;
+        }
+    }
+    Ok(())
+}
+
+pub fn main() -> Result<(), anyhow::Error> {
+    let server = Server::http("127.0.0.1:8000").unwrap();
+
+    let conn = rusqlite::Connection::open_in_memory()?;
+
+    file_server::init_db(&conn)?;
+
+    for request in server.incoming_requests() {
+        println!(
+            "received request! method: {:?}, url: {:?}, headers: {:?}",
+            request.method(),
+            request.url(),
+            request.headers()
+        );
+
+        let url = Url::parse(&format!("http://127.0.0.1:8000{}", request.url()));
+        if url.is_err() {
+            send_400_error("Can't parse URL", request)?;
+            continue;
+        }
+
+        let url = url.unwrap();
+
+        let queries: HashMap<String, String> = url
+            .query_pairs()
+            .into_iter()
+            .map(|(key, val)| (key.into_owned(), val.into_owned()))
+            .collect();
+
+        if url.path() == "/projects" {
+            let resp =
+                file_server::list_projects(&conn).map(|resp| serde_json::to_string(&resp).unwrap());
+            respond_200_or_500(resp, request)?;
+            continue;
+        }
+
+        if url.path() == "/add-project" {
+            let path = queries.get("path");
+            if path.is_none() {
+                send_400_missing("path", request)?;
+                continue;
+            }
+
+            let path = path.unwrap();
+            let proj = file_server::add_project(path, &conn)
+                .map(|proj| serde_json::to_string(&proj).unwrap());
+            respond_200_or_500(proj, request)?;
+            continue;
+        }
+
+        if url.path() == "/list-files" {
+            let project = queries.get("project");
+            if project.is_none() {
+                send_400_missing("project", request)?;
+                continue;
+            }
+
+            let project = str::parse::<u64>(project.unwrap());
+            if project.is_err() {
+                send_400_error("Can't parse project", request)?;
+                continue;
+            }
+            let project = project.unwrap();
+
+            let path = queries.get("path");
+            if path.is_none() {
+                send_400_missing("path", request)?;
+                continue;
+            }
+            let path = path.unwrap();
+
+            let files = file_server::list_files(project, path, &conn)
+                .map(|files| serde_json::to_string(&files).unwrap());
+            respond_200_or_500(files, request)?;
+            continue;
+        }
+
+        let response = Response::from_string("hello world");
+        request.respond(response).unwrap();
+    }
+
+    Ok(())
+}
