6c9460e3919736f2db8353c64cc2fd34f7b08d56
Yuan Fu <casouri@gmail.com>
2025-08-03 19:38:49 -0700

    Allow accepting on multuple signaling servers

--- collab_mode/src/webchannel.rs
+++ collab_mode/src/webchannel.rs
@@ -99,8 +99,9 @@ pub struct WebChannel {
     msg_tx: mpsc::Sender<Message>,
     assoc_tx: Arc<Mutex<HashMap<ServerId, mpsc::Sender<Message>>>>,
     next_stream_id: Arc<AtomicU16>,
-    /// Wether there’s an accepting thread running already.
-    accepting: bool,
+    /// Wether there’s an accepting thread running already for a
+    /// signaling server URL.
+    accepting: Arc<Mutex<HashMap<String, Arc<Mutex<()>>>>>,
 }
 
 impl WebChannel {
@@ -110,7 +111,7 @@ impl WebChannel {
             msg_tx,
             assoc_tx: Arc::new(Mutex::new(HashMap::new())),
             next_stream_id: Arc::new(AtomicU16::new(STREAM_ID_INIT)),
-            accepting: false,
+            accepting: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
@@ -126,13 +127,23 @@ impl WebChannel {
         my_key_cert: ArcKeyCert,
         signaling_addr: &str,
     ) -> anyhow::Result<()> {
-        if self.accepting {
-            return Err(anyhow!(WebchannelError::AlreadyAccepting));
+        let accepting = self.accepting.lock().unwrap().get(signaling_addr).map_or(None, |x| Some(x.clone()));
+
+        match accepting {
+            None => {
+                let lock = Arc::new(Mutex::new(()));
+                lock.lock().unwrap();
+                self.accepting.lock().unwrap().insert(signaling_addr.to_string(), lock.clone());
+                self.accept_inner(my_key_cert, signaling_addr).await
+            }
+            Some(lock) => {
+                match lock.try_lock() {
+                    Ok(_) => self.accept_inner(my_key_cert, signaling_addr).await,
+                    Err(_) => Err(anyhow!(WebchannelError::AlreadyAccepting)),
+                }
+            }
         }
-        self.accepting = true;
-        let res = self.accept_inner(my_key_cert, signaling_addr).await;
-        self.accepting = false;
-        return res;
+
     }
 
     pub async fn accept_inner(
