3df9dae64a847b7b6ceaeb178f3a4bb1d13ded22
Yuan Fu <casouri@gmail.com>
2025-08-20 23:19:05 -0700

    Add test for trusted hosts

--- collab_mode/src/webchannel/e2e_tests.rs
+++ collab_mode/src/webchannel/e2e_tests.rs
@@ -855,4 +855,547 @@ mod e2e_tests {
 
         accept_handle.abort();
     }
+
+    #[tokio::test]
+    async fn test_accept_mode_trusted_only_accepts_trusted_host() {
+        let env = TestEnvironment::new().await.unwrap();
+
+        let (tx1, mut rx1) = mpsc::channel::<Message>(100);
+        let (tx2, _rx2) = mpsc::channel::<Message>(100);
+
+        let id1 = create_test_id("trusted-1");
+        let id2 = create_test_id("trusted-2");
+
+        // Create key/cert pairs
+        let key_cert1 = create_test_key_cert(&id1);
+        let key_cert2 = create_test_key_cert(&id2);
+
+        // Get cert hash for channel2
+        let cert_hash2 = key_cert2.cert_der_hash();
+
+        // Create channel1 with TrustedOnly mode and channel2 in trusted_hosts
+        let mut trusted_hosts = HashMap::new();
+        trusted_hosts.insert(id2.clone(), cert_hash2);
+
+        let channel1 = WebChannel::new(
+            id1.clone(),
+            tx1,
+            Arc::new(Mutex::new(trusted_hosts)),
+            Arc::new(Mutex::new(AcceptMode::TrustedOnly)),
+        );
+
+        let channel2 = WebChannel::new(
+            id2.clone(),
+            tx2,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Start accepting on channel1
+        let accept_handle = {
+            let mut channel1 = channel1.clone();
+            let key_cert1 = key_cert1.clone();
+            let signaling_url = env.signaling_url().to_string();
+            tokio::spawn(async move {
+                let _ = channel1
+                    .accept(key_cert1, &signaling_url, TransportType::SCTP)
+                    .await;
+            })
+        };
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Connect from channel2 (trusted host) - should succeed
+        let connect_result = timeout(
+            Duration::from_secs(5),
+            channel2.connect(
+                id1.clone(),
+                id2.clone(),
+                key_cert2.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_ok() && connect_result.unwrap().is_ok(),
+            "Connection from trusted host should succeed"
+        );
+
+        // Exchange messages to verify connection
+        channel2
+            .send(&id1, None, Msg::FileShared(42))
+            .await
+            .unwrap();
+
+        // Skip any Hey messages that might arrive first
+        let mut received_file_shared = false;
+        for _ in 0..5 {
+            if let Ok(Some(msg)) = timeout(Duration::from_secs(1), rx1.recv()).await {
+                assert_eq!(msg.host, id2);
+                if let Msg::FileShared(doc_id) = msg.body {
+                    assert_eq!(doc_id, 42);
+                    received_file_shared = true;
+                    break;
+                }
+                // Skip other messages like Hey
+            }
+        }
+        assert!(received_file_shared, "Should receive FileShared message");
+
+        accept_handle.abort();
+    }
+
+    #[tokio::test]
+    async fn test_accept_mode_trusted_only_rejects_untrusted_host() {
+        let env = TestEnvironment::new().await.unwrap();
+
+        let (tx1, _rx1) = mpsc::channel::<Message>(100);
+        let (tx2, _rx2) = mpsc::channel::<Message>(100);
+
+        let id1 = create_test_id("untrusted-1");
+        let id2 = create_test_id("untrusted-2");
+
+        // Create key/cert pairs
+        let key_cert1 = create_test_key_cert(&id1);
+        let key_cert2 = create_test_key_cert(&id2);
+
+        // Create channel1 with TrustedOnly mode and empty trusted_hosts
+        let channel1 = WebChannel::new(
+            id1.clone(),
+            tx1,
+            Arc::new(Mutex::new(HashMap::new())), // Empty trusted hosts
+            Arc::new(Mutex::new(AcceptMode::TrustedOnly)),
+        );
+
+        let channel2 = WebChannel::new(
+            id2.clone(),
+            tx2,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Start accepting on channel1
+        let accept_handle = {
+            let mut channel1 = channel1.clone();
+            let key_cert1 = key_cert1.clone();
+            let signaling_url = env.signaling_url().to_string();
+            tokio::spawn(async move {
+                let _ = channel1
+                    .accept(key_cert1, &signaling_url, TransportType::SCTP)
+                    .await;
+            })
+        };
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Connect from channel2 (untrusted host) - should fail
+        let connect_result = timeout(
+            Duration::from_secs(3),
+            channel2.connect(
+                id1.clone(),
+                id2.clone(),
+                key_cert2.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        // Connection should either timeout or return an error
+        assert!(
+            connect_result.is_err() || connect_result.unwrap().is_err(),
+            "Connection from untrusted host should fail"
+        );
+
+        accept_handle.abort();
+    }
+
+    #[tokio::test]
+    async fn test_accept_mode_all_accepts_any_host() {
+        let env = TestEnvironment::new().await.unwrap();
+
+        let (tx1, mut rx1) = mpsc::channel::<Message>(100);
+        let (tx2, _rx2) = mpsc::channel::<Message>(100);
+
+        let id1 = create_test_id("all-1");
+        let id2 = create_test_id("all-2");
+
+        // Create key/cert pairs
+        let key_cert1 = create_test_key_cert(&id1);
+        let key_cert2 = create_test_key_cert(&id2);
+
+        // Get cert hash for channel2
+        let cert_hash2 = key_cert2.cert_der_hash();
+
+        // Create channel1 with All mode and empty trusted_hosts
+        let trusted_hosts1 = Arc::new(Mutex::new(HashMap::new()));
+        let channel1 = WebChannel::new(
+            id1.clone(),
+            tx1,
+            trusted_hosts1.clone(),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        let channel2 = WebChannel::new(
+            id2.clone(),
+            tx2,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Start accepting on channel1
+        let accept_handle = {
+            let mut channel1 = channel1.clone();
+            let key_cert1 = key_cert1.clone();
+            let signaling_url = env.signaling_url().to_string();
+            tokio::spawn(async move {
+                let _ = channel1
+                    .accept(key_cert1, &signaling_url, TransportType::SCTP)
+                    .await;
+            })
+        };
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Connect from channel2 - should succeed even without being in trusted_hosts
+        let connect_result = timeout(
+            Duration::from_secs(5),
+            channel2.connect(
+                id1.clone(),
+                id2.clone(),
+                key_cert2.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_ok() && connect_result.unwrap().is_ok(),
+            "Connection should succeed with AcceptMode::All"
+        );
+
+        // Exchange messages to verify connection
+        channel2
+            .send(&id1, None, Msg::FileShared(99))
+            .await
+            .unwrap();
+        let msg = timeout(Duration::from_secs(2), rx1.recv())
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(msg.host, id2);
+
+        // Verify that channel2's cert was added to trusted_hosts
+        sleep(Duration::from_millis(100)).await;
+        let trusted = trusted_hosts1.lock().unwrap();
+        assert!(
+            trusted.contains_key(&id2),
+            "Host should be added to trusted_hosts"
+        );
+        assert_eq!(
+            trusted.get(&id2).unwrap(),
+            &cert_hash2,
+            "Cert hash should match"
+        );
+
+        accept_handle.abort();
+    }
+
+    #[tokio::test]
+    async fn test_dynamic_trusted_hosts_update() {
+        let env = TestEnvironment::new().await.unwrap();
+
+        let (tx1, mut rx1) = mpsc::channel::<Message>(100);
+        let (tx2, _rx2) = mpsc::channel::<Message>(100);
+        let (tx3, _rx3) = mpsc::channel::<Message>(100);
+
+        let id1 = create_test_id("dynamic-1");
+        let id2 = create_test_id("dynamic-2");
+        let id3 = create_test_id("dynamic-3"); // Different ID for second attempt
+
+        // Create key/cert pairs
+        let key_cert1 = create_test_key_cert(&id1);
+        let key_cert2 = create_test_key_cert(&id2);
+        let key_cert3 = create_test_key_cert(&id3);
+
+        // Get cert hashes
+        let cert_hash2 = key_cert2.cert_der_hash();
+        let cert_hash3 = key_cert3.cert_der_hash();
+
+        // Create channel1 with TrustedOnly mode and empty trusted_hosts
+        let trusted_hosts1 = Arc::new(Mutex::new(HashMap::new()));
+        let channel1 = WebChannel::new(
+            id1.clone(),
+            tx1,
+            trusted_hosts1.clone(),
+            Arc::new(Mutex::new(AcceptMode::TrustedOnly)),
+        );
+
+        let channel2 = WebChannel::new(
+            id2.clone(),
+            tx2,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Start accepting on channel1
+        let accept_handle = {
+            let mut channel1 = channel1.clone();
+            let key_cert1 = key_cert1.clone();
+            let signaling_url = env.signaling_url().to_string();
+            tokio::spawn(async move {
+                loop {
+                    let _ = channel1
+                        .accept(key_cert1.clone(), &signaling_url, TransportType::SCTP)
+                        .await;
+                    sleep(Duration::from_millis(100)).await;
+                }
+            })
+        };
+
+        sleep(Duration::from_millis(200)).await;
+
+        // First connection attempt from channel2 - should fail (not trusted)
+        let connect_result = timeout(
+            Duration::from_secs(2),
+            channel2.connect(
+                id1.clone(),
+                id2.clone(),
+                key_cert2.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_err() || connect_result.unwrap().is_err(),
+            "First connection should fail (not trusted)"
+        );
+
+        // Dynamically add channel3 (with same cert pattern) to trusted_hosts
+        trusted_hosts1
+            .lock()
+            .unwrap()
+            .insert(id3.clone(), cert_hash3);
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Create channel3 with new ID but will be trusted
+        let channel3 = WebChannel::new(
+            id3.clone(),
+            tx3,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Connection attempt from channel3 - should succeed (now trusted)
+        let connect_result = timeout(
+            Duration::from_secs(5),
+            channel3.connect(
+                id1.clone(),
+                id3.clone(),
+                key_cert3.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_ok() && connect_result.unwrap().is_ok(),
+            "Second connection should succeed (now trusted)"
+        );
+
+        // Exchange messages to verify connection
+        channel3
+            .send(&id1, None, Msg::FileShared(77))
+            .await
+            .unwrap();
+
+        // Skip any Hey messages that might arrive first
+        let mut received_file_shared = false;
+        for _ in 0..5 {
+            if let Ok(Some(msg)) = timeout(Duration::from_secs(1), rx1.recv()).await {
+                assert_eq!(msg.host, id3);
+                if let Msg::FileShared(doc_id) = msg.body {
+                    assert_eq!(doc_id, 77);
+                    received_file_shared = true;
+                    break;
+                }
+                // Skip other messages like Hey
+            }
+        }
+        assert!(received_file_shared, "Should receive FileShared message");
+
+        accept_handle.abort();
+    }
+
+    #[tokio::test]
+    async fn test_switch_accept_mode_from_all_to_trusted_only() {
+        let env = TestEnvironment::new().await.unwrap();
+
+        let (tx1, mut rx1) = mpsc::channel::<Message>(100);
+        let (tx2, _rx2) = mpsc::channel::<Message>(100);
+        let (tx3, _rx3) = mpsc::channel::<Message>(100);
+        let (tx4, _rx4) = mpsc::channel::<Message>(100);
+
+        let id1 = create_test_id("switch-1");
+        let id2 = create_test_id("switch-2");
+        let id3 = create_test_id("switch-3");
+        let id4 = create_test_id("switch-4"); // New ID for reconnection with trusted cert
+
+        // Create key/cert pairs
+        let key_cert1 = create_test_key_cert(&id1);
+        let key_cert2 = create_test_key_cert(&id2);
+        let key_cert3 = create_test_key_cert(&id3);
+        // For channel4, we'll use the same cert as channel2
+        let cert_hash2 = key_cert2.cert_der_hash();
+
+        // Create channel1 with All mode initially
+        let trusted_hosts1 = Arc::new(Mutex::new(HashMap::new()));
+        let accept_mode1 = Arc::new(Mutex::new(AcceptMode::All));
+        let channel1 = WebChannel::new(
+            id1.clone(),
+            tx1,
+            trusted_hosts1.clone(),
+            accept_mode1.clone(),
+        );
+
+        let channel2 = WebChannel::new(
+            id2.clone(),
+            tx2,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        let channel3 = WebChannel::new(
+            id3.clone(),
+            tx3,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Start accepting on channel1
+        let accept_handle = {
+            let mut channel1 = channel1.clone();
+            let key_cert1 = key_cert1.clone();
+            let signaling_url = env.signaling_url().to_string();
+            tokio::spawn(async move {
+                loop {
+                    let _ = channel1
+                        .accept(key_cert1.clone(), &signaling_url, TransportType::SCTP)
+                        .await;
+                    sleep(Duration::from_millis(100)).await;
+                }
+            })
+        };
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Connect from channel2 with AcceptMode::All - should succeed
+        let connect_result = timeout(
+            Duration::from_secs(5),
+            channel2.connect(
+                id1.clone(),
+                id2.clone(),
+                key_cert2.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_ok() && connect_result.unwrap().is_ok(),
+            "Connection should succeed with AcceptMode::All"
+        );
+
+        // Verify connection works
+        channel2
+            .send(&id1, None, Msg::FileShared(88))
+            .await
+            .unwrap();
+
+        // Skip any Hey messages that might arrive first
+        let mut received_file_shared = false;
+        for _ in 0..5 {
+            if let Ok(Some(msg)) = timeout(Duration::from_secs(1), rx1.recv()).await {
+                assert_eq!(msg.host, id2);
+                if let Msg::FileShared(doc_id) = msg.body {
+                    assert_eq!(doc_id, 88);
+                    received_file_shared = true;
+                    break;
+                }
+                // Skip other messages like Hey
+            }
+        }
+        assert!(received_file_shared, "Should receive FileShared message");
+
+        // Disconnect channel2
+        channel1.assoc_tx.lock().unwrap().remove(&id2);
+        channel2.assoc_tx.lock().unwrap().remove(&id1);
+        sleep(Duration::from_millis(200)).await;
+
+        // Switch to TrustedOnly mode
+        *accept_mode1.lock().unwrap() = AcceptMode::TrustedOnly;
+
+        // The cert from channel2 should now be in trusted_hosts
+        // Let's manually add it with a different host ID that will be used for reconnection
+        trusted_hosts1
+            .lock()
+            .unwrap()
+            .insert(id4.clone(), cert_hash2.clone());
+
+        sleep(Duration::from_millis(200)).await;
+
+        // Try to connect from channel3 (not trusted) - should fail
+        let connect_result = timeout(
+            Duration::from_secs(2),
+            channel3.connect(
+                id1.clone(),
+                id3.clone(),
+                key_cert3.clone(),
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_err() || connect_result.unwrap().is_err(),
+            "Connection from untrusted host should fail after switching to TrustedOnly"
+        );
+
+        // Create channel4 with new ID but same cert as channel2 (trusted)
+        let channel4 = WebChannel::new(
+            id4.clone(),
+            tx4,
+            Arc::new(Mutex::new(HashMap::new())),
+            Arc::new(Mutex::new(AcceptMode::All)),
+        );
+
+        // Try to connect from channel4 (trusted cert, different ID) - should succeed
+        let connect_result = timeout(
+            Duration::from_secs(5),
+            channel4.connect(
+                id1.clone(),
+                id4.clone(),
+                key_cert2.clone(), // Using same cert as channel2
+                env.signaling_url(),
+                TransportType::SCTP,
+            ),
+        )
+        .await;
+
+        assert!(
+            connect_result.is_ok() && connect_result.unwrap().is_ok(),
+            "Connection from host with trusted certificate should succeed"
+        );
+
+        accept_handle.abort();
+    }
 }
